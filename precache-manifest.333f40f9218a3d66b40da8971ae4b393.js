6157ce11bc9919b635b621772b462669c79e0f7d
raphaelroques <raphaelroques@gmail.com>
2020-09-28 14:08:15 -0400

    Updates

---
+++ precache-manifest.333f40f9218a3d66b40da8971ae4b393.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31beedb40d1bdf34f4cce5efcc5b957a",
+    "url": "/raphael-roques-project-five/index.html"
+  },
+  {
+    "revision": "2afbc58117d64f7cfcba",
+    "url": "/raphael-roques-project-five/static/css/main.00415dfe.chunk.css"
+  },
+  {
+    "revision": "215851e166c522388193",
+    "url": "/raphael-roques-project-five/static/js/2.62b2023f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/raphael-roques-project-five/static/js/2.62b2023f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2afbc58117d64f7cfcba",
+    "url": "/raphael-roques-project-five/static/js/main.49cc02a7.chunk.js"
+  },
+  {
+    "revision": "f9094c52249c8aa5e398",
+    "url": "/raphael-roques-project-five/static/js/runtime-main.635b13dd.js"
+  },
+  {
+    "revision": "a97ee7b506c55ddf70806766273683c4",
+    "url": "/raphael-roques-project-five/static/media/landing-page-image.a97ee7b5.jpg"
+  }
+]);
